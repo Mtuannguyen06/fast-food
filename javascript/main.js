5e3cb4314ffbc931268882297eeb29947c52251c
Mtuannguyen06 <minhtuannguyennhu@gamil.com>
2024-01-14 15:59:38 +0700

    14a

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+let productList = [];
+
+const productListElm = document.getElementById("product-list");
+const productTemp = document.getElementById("product-template");
+
+function getProductList() {
+  fetch("https://6578608ef08799dc80451942.mockapi.io/pro-duck")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      productList = data;
+      renderProductList();
+    });
+}
+
+function renderProductList() {
+  for (let index = 0; index < productList.length; index++) {
+    const product = productList[index];
+    const productItem = productTemp.content.cloneNode(true);
+    productItem.getElementById("product-image").src = product.image;
+    productItem.getElementById("product-name").textContent = product.title;
+    productItem.getElementById("product-price").textContent = product.price;
+    productListElm.appendChild(productItem);
+  }
+}
+
+getProductList();
