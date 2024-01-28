58e4175f4a4884b2390c00ce7e29188dbeb7264b
Mtuannguyen06 <minhtuannguyennhu@gamil.com>
2024-01-28 14:47:41 +0700

    na

--- javascript/main.js
+++ javascript/main.js
@@ -25,3 +25,31 @@ function renderProductList() {
 }
 
 getProductList();
+
+const producctTemp = document.getElementById("product-template-c");
+const productListcElm = document.getElementById("product-list-c");
+
+function getProductListc() {
+  fetch("https://6578608ef08799dc80451942.mockapi.io/new")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      productList = data;
+      renderProductListc();
+    });
+}
+
+function renderProductListc() {
+  for (let index = 18; index < 21; index++) {
+    const product = productList[index];
+    const productItem = producctTemp.content.cloneNode(true);
+    productItem.getElementById("product-image").src = product.image;
+    productItem.getElementById("product-name").textContent = product.title;
+    productItem.getElementById("product-price").textContent = product.price;
+    productItem.getElementById("product-text").textContent = product.text;
+    productListcElm.appendChild(productItem);
+  }
+}
+
+getProductListc();
